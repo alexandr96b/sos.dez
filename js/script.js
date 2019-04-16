e394f225a06e5d0c41f593999443a9ab2c474715
Alex Bondarenko <alexandr96b@gmail.com>
2019-04-16 16:12:32 +0300

    add certificates

--- js/script.js
+++ js/script.js
@@ -49,6 +49,46 @@ $('.b-foto-slider').slick({
 });
 
 
+
+$('.b-certificates-content > .row').slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    fade: false,
+    variableWidth: false,
+    arrows: true,
+    dots: false,
+    nextArrow: '<button class="b-arrow b-arrow__next">→</button>',
+    prevArrow: '<button class="b-arrow b-arrow__prev">←</button>',  
+    appendArrows: '.b-certificates-nav',   
+    responsive: [
+  {
+    breakpoint: 1200,
+    settings: {
+      slidesToShow: 4,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 992,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 768,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+  // You can unslick at a given breakpoint now by adding:
+  // settings: "unslick"
+  // instead of a settings object
+]
+});
+
+
 $(".b-nav__wrap a").click(function (event) {
     event.preventDefault();
     var id  = $(this).attr('href'),
